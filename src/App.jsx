461b07159cee56d9ac30dfd2c0f1967e3106993a
marisqra <marisqra@gmail.com>
2025-06-16 18:36:22 -0300

    organização do código

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,36 @@ import Footer from "./componentes/footer/Footer"
 
 function App() {
 
-
   return (
     <>
-    <NavBar />
-      <h1>Explore a Galeria</h1>
-      <p>Explore o acervo de forma diferente. Cada tag é um convite à descoberta,
-         experimente combinações e veja a galeria se transformar.</p>
-      <Botao/>
-      <div class="card-container">
-        <CardObra />
-        <CardObra />
-        <CardObra />
-        <CardObra />
-        <CardObra />
-        <CardObra />
-      </div>
-      
-      <Footer/>
- 
-      
+      <NavBar />
+      <section class="hero-section">
+        <div>
+          <h1>Explore a Galeria</h1>
+          <p>Explore o acervo de forma diferente. Cada tag é um convite à descoberta,
+            experimente combinações e veja a galeria se transformar.</p>
+          <Botao />
+        </div>
+        <img src="public/assets/Homi.svg" />
+      </section>
+      <section class="galery">
+        <div class="card-container">
+          <CardObra />
+          <CardObra />
+          <CardObra />
+          <CardObra />
+          <CardObra />
+          <CardObra />
+        </div>
+      </section>
+
+      <section class="footer-section">
+        <img src="public/assets/Onda.svg" alt="" />
+        <Footer />
+      </section>
+
+
+
     </>
   )
 }
