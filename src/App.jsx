1e2efe2ac6d5256e46ea40f33e060a6561859346
marisqra <marisqra@gmail.com>
2025-07-04 17:34:05 -0300

    criação das primeiras rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import NavBar from "./componentes/NavBar/NavBar"
-import CardObra from "./componentes/Card-obra/Card-obra"
-import Botao from "./componentes/botão/Botao"
 import "./App.css"
-import Footer from "./componentes/footer/Footer"
+import Home from "./pages/Home/Home"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Admin from "./pages/Admin/Admin"
 
 
 
@@ -11,33 +10,17 @@ function App() {
 
   return (
     <>
-      <NavBar />
-      <section class="hero-section">
-        <div>
-          <h1>Explore a Galeria</h1>
-          <p>Explore o acervo de forma diferente. Cada tag é um convite à descoberta,
-            experimente combinações e veja a galeria se transformar.</p>
-          <Botao />
-        </div>
-        <img src="public/assets/Homi.svg" />
-      </section>
-      <section class="galery">
-        <div class="card-container">
-          <CardObra />
-          <CardObra />
-          <CardObra />
-          <CardObra />
-          <CardObra />
-          <CardObra />
-        </div>
-      </section>
+      <ul>
+        <a href={"/"}>Home</a>
+        <a href={"admin"}>Admin</a>
+      </ul>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/admin' element={<Admin />} />
+      </Routes>
 
-      <section class="footer-section">
-        <img src="public/assets/Onda.svg" alt="" />
-        <Footer />
-      </section>
-
-
+    </BrowserRouter>
 
     </>
   )
