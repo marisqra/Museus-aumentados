f5bb27637046c65255dc94a982fa063f7a4198c7
marisqra <marisqra@gmail.com>
2025-07-06 17:11:46 -0300

    commit dnv pq bugou no meu

--- src/componentes/footer/Footer.jsx
+++ src/componentes/footer/Footer.jsx
@@ -1,4 +1,4 @@
-//nico que fez o commit mas bugou no meu!!
+//nico colocou o icone mas bugou no meu 
 
 import "./Footer.css";
 import { MdEmail, MdPhone } from "react-icons/md";
@@ -16,39 +16,8 @@ export default function Rodape() {
 
           <div className="contato-rodape">
             <div className="contato-item">
-               <MdEmail className="footer-icon" />
-               <span>jaovinei@gmail.com</span>
-            </div>
-            <div className="contato-item">
-              <MdPhone className="footer-icon" />
-              <span>(88) 40028922</span>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <hr className="divisor-rodape" />
-      <p className="copy-rodape">©2025, Todos os direitos reservados.</p>
-    </footer>
-  );
-}import "./Footer.css";
-import { MdEmail, MdPhone } from "react-icons/md";
-
-export default function Rodape() {
-  return (
-    <footer className="rodape">
-      <div className="conteudo-rodape">
-        <div className="logo-rodape"></div>
-
-        <div className="info-rodape">
-          <p className="descricao-rodape">
-            Para dúvidas, sugestões ou parcerias, entre em contato.
-          </p>
-
-          <div className="contato-rodape">
-            <div className="contato-item">
-               <MdEmail className="footer-icon" />
-               <span>jaovinei@gmail.com</span>
+              <MdEmail className="footer-icon" />
+              <span>jaovinei@gmail.com</span>
             </div>
             <div className="contato-item">
               <MdPhone className="footer-icon" />
